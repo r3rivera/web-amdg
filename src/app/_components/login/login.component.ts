6c868d16ec919e8ab55ff323ffffc4adfdef4050
Ryan Rivera <r.ryan.rivera@outlook.com>
2020-05-27 23:56:34 -0500

    Testing service and component bindings

--- src/app/_components/login/login.component.ts
+++ src/app/_components/login/login.component.ts
@@ -1,13 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from '@/_service';
+
 
 @Component({
   selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  templateUrl: './login.component.html'
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private authService : AuthenticationService
+  ) { 
+
+    console.log("Login Component...")
+
+  }
 
   ngOnInit(): void {
   }
