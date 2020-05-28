57a729a6bc32101e6fe16c5858634091b357b814
Ryan Rivera <r.ryan.rivera@outlook.com>
2020-05-28 00:48:21 -0500

    Added alert service and test image

--- src/app/_components/alert/alert.component.ts
+++ src/app/_components/alert/alert.component.ts
@@ -1,9 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-@Component({
-  selector: 'r3app-alert',
-  templateUrl: './alert.component.html'
-})
+@Component({ selector: 'r3app-alert', templateUrl: './alert.component.html'})
 export class AlertComponent implements OnInit {
 
   constructor() { }
