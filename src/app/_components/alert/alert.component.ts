7c59bded17edc15e72b2646678ec55b13c2fa149
Ryan Rivera <r.ryan.rivera@outlook.com>
2020-05-31 22:25:38 -0500

    Fix exception if subscription is undefined

--- src/app/_components/alert/alert.component.ts
+++ src/app/_components/alert/alert.component.ts
@@ -1,39 +1,41 @@
 import { Component, OnInit } from '@angular/core';
 
-import { Subscription } from 'rxjs'
+import { Subscription } from 'rxjs';
 import { AlertService } from '@/_service/utilities/alert.service';
 import { AppAlert } from '@/_models';
 
-@Component({ selector: 'r3app-alert', templateUrl: './alert.component.html'})
+@Component({ selector: 'r3app-alert', templateUrl: './alert.component.html' })
 export class AlertComponent implements OnInit {
+  private subscription: Subscription;
+  appAlert: AppAlert;
 
-  private subscription : Subscription
-  appAlert : AppAlert;
-
-  constructor(private alertService : AlertService) { }
-
-  ngOnInit() {
-    console.log("AlertComponent.ngOnInit() :: START");
-    this.subscription = this.alertService.getAlert()
-        .subscribe(msgAlert => {
-          console.log(`AlertComponent resp 1 :: ${JSON.stringify(msgAlert)}`);
-            switch (msgAlert && msgAlert.type) {
-                case 'success':
-                    msgAlert.style = 'alert alert-success';
-                    break;
-                case 'error':
-                    msgAlert.style = 'alert alert-danger';
-                    break;
-        }
-    
-        this.appAlert = msgAlert;
-        console.log(`AlertComponent resp 2 :: ${JSON.stringify(this.appAlert)}`);
-    });
-    console.log("AlertComponent.ngOnInit() :: END");
-  } 
-
-  ngOnDestroy() {
-        this.subscription.unsubscribe();
+  constructor(private alertService: AlertService) {
+    console.log('AlertComponent.constructor()');
   }
 
+  ngOnInit() {
+    console.log('AlertComponent.ngOnInit() :: START');
+    this.subscription = this.alertService.getAlert().subscribe((msgAlert) => {
+      console.log(`AlertComponent resp 1 :: ${JSON.stringify(msgAlert)}`);
+      switch (msgAlert && msgAlert.type) {
+        case 'success':
+          msgAlert.style = 'alert alert-success';
+          break;
+        case 'error':
+          msgAlert.style = 'alert alert-danger';
+          break;
+      }
+
+      this.appAlert = msgAlert;
+      console.log(`AlertComponent resp 2 :: ${JSON.stringify(this.appAlert)}`);
+    });
+
+    console.log('AlertComponent.ngOnInit() :: END');
+  }
+
+  ngOnDestroy() {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
+  }
 }
--- src/app/_components/secured/dashboard/dashboard.component.ts
+++ src/app/_components/secured/dashboard/dashboard.component.ts
@@ -1,14 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'r3app-dashboard',
   templateUrl: './dashboard.component.html'
 })
-export class DashboardComponent implements OnInit {
+export class DashboardComponent implements OnInit, OnDestroy {
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  ngOnDestroy(): void {
+    console.log("Dashboard Component Destroy!");
+    
   }
 
 }
